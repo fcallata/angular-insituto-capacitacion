a657ea9193600070ddb4bfa925d34c018328042f
Fabio Callata <fcallata@gmail.com>
2021-04-27 10:21:06 -0300

    agrego proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GaleriaComponent } from './pages/galeria/galeria.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegistreseComponent } from './pages/registrese/registrese.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'galeria', component: GaleriaComponent },
+  { path: 'registrese', component: RegistreseComponent },
+  { path: 'nosotros', component: RegistreseComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { GaleriaComponent } from './pages/galeria/galeria.component';
+import { RegistreseComponent } from './pages/registrese/registrese.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GaleriaComponent,
+    RegistreseComponent
   ],
   imports: [
     BrowserModule,
